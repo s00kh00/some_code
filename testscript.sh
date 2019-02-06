8f583793d7510f31b443e9fd8336b6bda951d94a
Neil Sookhoo <neilsookhoo@Neils-MacBook-Air.local>
2019-02-05 20:30:47 -0500

    testing hook

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 
-echo "this was run automagically, hells yeah"
+echo "this was run automagically, when I commmit"
